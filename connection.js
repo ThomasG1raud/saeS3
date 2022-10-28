8f621044661f2cde0af291b9ea3e7bde990e2e47
Ksisioss <thibault.lebreuil@gmail.com>
2022-10-28 10:13:17 +0200

    adding sequelize example

--- connection.js
+++ connection.js
@@ -7,3 +7,21 @@ const db = mysql.createConnection({
 })
 
 module.exports = db
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('TABLE', 'USERNAME', 'PASSWORD',{
+  dialect: 'mysql',
+  host: 'bdd2.adkynet.com',
+  port: '3306',
+});
+async function def(){
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+  const users = await sequelize.query("SELECT * FROM Cards");
+  console.log(users)
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}}
+
+def()

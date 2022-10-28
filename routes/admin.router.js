70864cda7e67f2d61a5a0ebd2a467b7ca7d185d2
Ksisioss <thibault.lebreuil@gmail.com>
2022-10-28 09:20:54 +0200

    correct route

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -4,21 +4,21 @@ const adminController = require("../controllers/admin.controller");
 router.get("/", adminController.panel)
 
 router.get('/prestataires', adminController.listPrestataires);
-router.get('/prestataires/:id?', adminController.idPrestataires);
-router.put('/prestataires/add', adminController.addPrestataires);
-router.post('/prestataires/update/:id', adminController.updatePrestataires);
+router.get('/prestataires/:id', adminController.idPrestataires);
+router.post('/prestataires/add', adminController.addPrestataires);
+router.put('/prestataires/update/:id', adminController.updatePrestataires);
 router.delete('/prestataires/delete/id:', adminController.deletePrestataires);
 
 router.get('/stands', adminController.listStands);
 router.get('/stands/:id', adminController.idStands);
-router.put('/stands/add', adminController.addStands);
-router.post('/stands/update/:id', adminController.updateStands);
+router.post('/stands/add', adminController.addStands);
+router.put('/stands/update/:id', adminController.updateStands);
 router.delete('/stands/delete/:id', adminController.deleteStands);
 
 router.get('/map', adminController.getMap)
 router.get('/map/:id', adminController.idMap)
-router.put('/map/add', adminController.addMap)
+router.post('/map/add', adminController.addMap)
 router.put('/map/update/:id', adminController.updateMap)
-router.put('/map/delete/:id', adminController.deleteMap)
+router.delete('/map/delete/:id', adminController.deleteMap)
 
 module.exports = router;

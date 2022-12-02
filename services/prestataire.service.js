e7b035836cfdecf55e5a0595f5df9b88813c09bf
aurelcode <63055821+aurelcode@users.noreply.github.com>
2022-12-02 09:52:02 +0100

    fonctions dans services pour prestataire, les tables+controller+routes sont à faire pour tout relier + modifier dans le mcd pour les incorporer

--- services/prestataire.service.js
+++ services/prestataire.service.js
@@ -27,6 +27,81 @@ const selfEdit = async (news,callback) => {
         })
 }
 
+const compteVisisteurs = async (callback) =>{
+    await pool.query("SELECT * FROM client WHERE state = connecte")
+        .then(results=>{
+            return (callback(null, results.rows))
+        })
+        .catch(error=>{
+            return (callback(error, null))
+        })
+}
+
+const livreDOr = async (news, callback) =>{
+    await pool.query("INSERT INTO livreDOr (nomClient, commentaire) VALUES ($1, $2)", [news[0], news[1]])
+        .then(results=>{
+            return callback(null, results.rows)
+        })
+        .catch(error=>{
+            return callback(error, null)
+        })
+}
+
+const achatBillet = async (news, callback)=>{
+    await pool.query("SELECT * FROM utilisateur WHERE nom = $1", [news[0]], (error, results)=>{
+        if(results.rows.length){
+            pool.query("INSERT INTO billet(prix,idUtilsateur, idCategory) VALUES ($1, $2, $3)", [news[1],results.rows[0], news[2]])
+                .then(results=>{
+                    return callback(null, results.rows)
+                })
+                .catch(error=>{
+                    return callback(error, null)
+                })
+        }
+        else{
+            pool.query("INSERT INTO utilisateur(nom) VALUES ($1) RETURNING idUtilisateur", [news[0]], (erreur, results)=>{
+                pool.query("INSERT INTO billet(prix,idUtilsateur, idCategory) VALUES ($1, $2, $3)", [news[1],results.rows[0], news[2]])
+                    .then(results=>{
+                        return callback(null, results.rows)
+                    })
+                    .catch(error=>{
+                        return callback(error, null)
+                    })
+            })
+        }
+    })
+}
+
+const showGoodies = async (idPrestataire, callback)=>{
+    await pool.query("SELECT * FROM goodies WHERE idPrestataire = $1", [idPrestataire])
+        .then(results=>{
+            return callback(null, results.rows)
+        })
+        .catch(error=>{
+            return callback(error, null)
+        })
+}
+
+const selectGoodies = async(news, callback)=>{
+    await pool.query("INSERT INTO panier(idGoodies, idUtilisateur) VALUES ($1, $2)", [news[0], news[1]])
+        .then(results=>{
+            return callback(null, results.rows)
+        })
+        .catch(error=>{
+            return callback(error, null)
+        })
+}
+
+const buyGoodies = async(news, callback)=>{
+    await pool.query("SELECT * FROM panier WHERE idUtilisateur = $1", [news[0]])
+        .then(results=>{
+            return callback(null, results.rows)
+        })
+        .catch(error=>{
+            return callback(error, null)
+        })
+}
+
 module.exports = {
     panel: panel,
     idStatistiques : idStatistiques,

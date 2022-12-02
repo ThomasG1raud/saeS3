2d8dbba06e0f7f8c38da3b376cb4a9522523fa4b
ThomasG1raud <thomas.giraud@edu.univ-fcomte.fr>
2022-12-02 08:47:26 +0100

    vitrine service

--- services/vitrine.service.js
+++ services/vitrine.service.js
@@ -9,24 +9,44 @@ const map = (callback) => {
     return callback(null, "ok");
 }
 
-const listePrestataire = (callback) => {
-    const prestataires = pool.query("SELECT * FROM prestataires");
-    return callback(null, prestataires);
+const listePrestataire = async (callback) => {
+    await pool.query("SELECT * FROM prestataires")
+    .then(results=>{
+        return callback(null, results.rows)
+    })
+    .catch(error=>{
+        return callback(error, null)
+    })
 }
 
-const idPrestataire = (callback) => {
-    const prestataire = pool.query("SELECT * FROM prestataires WHERE idprestataire = $1", [id]);
-    return callback(null, prestataire);
+const idPrestataire = async (id, callback) => {
+    await pool.query("SELECT * FROM prestataires WHERE idprestataire = $1", [id])
+    .then(results=>{
+        return callback(null, results.rows)
+    })
+    .catch(error=>{
+        return callback(error, null)
+    })
 }
 
-const listeStand = (callback) => {
-    const stands = pool.query("SELECT * FROM emplacements");
-    return callback(null, stands);
+const listeStand = async (callback) => {
+    await pool.query("SELECT * FROM emplacements")
+    .then(results=>{
+        return callback(null, results.rows)
+    })
+    .catch(error=>{
+        return callback(error, null)
+    })
 }
 
-const idStand = (callback) => {
-    const stand = pool.query("SELECT * FROM prestataires WHERE idprestataire = $1", [id]);
-    return callback(null, stand);
+const idStand = async (id,callback) => {
+    await pool.query("SELECT * FROM prestataires WHERE idprestataire = $1", [id])
+    .then(results=>{
+        return callback(null, results.rows)
+    })
+    .catch(error=>{
+        return callback(error, null)
+    })
 }
 
 

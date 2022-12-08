a5fd6475490b8de77a679d4b01c58c906d50b27a
ThomasG1raud <thomas.giraud@edu.univ-fcomte.fr>
2022-12-08 14:25:15 +0100

    modifs prestataire queries service

--- services/prestataire.service.js
+++ services/prestataire.service.js
@@ -6,7 +6,7 @@ const panel = (callback) => {
 }
 
 const idStatistiques = (id,callback) => {
-    pool.query(prestataireQuery.selectById)
+    pool.query(prestataireQuery.selectPrestataireById)
         .then(results=>{
             return callback(null, results.rows)
         })
@@ -29,7 +29,7 @@ const selfEdit = async (news,callback) => {
 }
 
 const compteVisisteurs = async (callback) =>{
-    await pool.query(prestataireQuery.compteClient)
+    await pool.query(prestataireQuery.countClient)
         .then(results=>{
             return (callback(null, results.rows))
         })
@@ -49,9 +49,9 @@ const livreDOr = async (news, callback) =>{
 }
 
 const achatBillet = async (news, callback)=>{
-    await pool.query(prestataireQuery.utilisateurNom, [news[0]], (error, results)=>{
+    await pool.query(prestataireQuery.selectUserByName, [news[0]], (error, results)=>{
         if(results.rows.length){
-            pool.query(prestataireQuery.ajouterBillet, [news[1],results.rows[0], news[2]])
+            pool.query(prestataireQuery.addBillet, [news[1],results.rows[0], news[2]])
                 .then(results=>{
                     return callback(null, results.rows)
                 })
@@ -60,8 +60,8 @@ const achatBillet = async (news, callback)=>{
                 })
         }
         else{
-            pool.query(prestataireQuery.ajouterUtilisateur, [news[0]], (erreur, results)=>{
-                pool.query(prestataireQuery.ajouterBillet, [news[1],results.rows[0], news[2]])
+            pool.query(prestataireQuery.addUser, [news[0]], (erreur, results)=>{
+                pool.query(prestataireQuery.addBillet, [news[1],results.rows[0], news[2]])
                     .then(results=>{
                         return callback(null, results.rows)
                     })

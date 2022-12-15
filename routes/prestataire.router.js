7c479732490bb09249059acaa90a71afdf3a8a85
ThomasG1raud <thomas.giraud@edu.univ-fcomte.fr>
2022-12-15 14:19:45 +0100

    tempo prestataire swagger

--- routes/prestataire.router.js
+++ routes/prestataire.router.js
@@ -4,124 +4,142 @@ const router = express.Router();
 
 /**
  * @swagger
- * /prestataire/ :
+ * / :
  *      get :
  *          description : Used to get general informations from the prestataire table
  *          tags :
  *              - prestataire
  *          parameters : none
  *          responses :
- *              - 200 : ressources récupérer avec succès
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.get("/", prestataireController.panel);
 /**
  * @swagger
- * /prestataire/statistiques :
+ * /statistiques/{id} :
  *      get :
  *          description : Used to show the selected stats
  *          tags :
  *              - prestataire
- *              - statistique
  *          parameters :
- *              - id wich represent the id of the selected stats for the corresponding prestataire
- *          responses :
- *              - 200 : ressources récupérer avec succès
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          - in: path
+ *            name: id
+ *            type: integer
+ *            required: false
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.get("/statistiques/:id", prestataireController.idStatistiques);
 /**
  * @swagger
- * /prestataire/self_edit :
- *      get :
+ * /self_edit :
+ *      put :
  *          description : Used by a prestataire to edit himself
  *          tags :
  *              - prestataire
  *          parameters : none
  *          responses :
- *              - 200 : prestataire got edited with success
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
-router.get("/self_edit", prestataireController.selfEdit);
+router.put("/self_edit", prestataireController.selfEdit);
 /**
  * @swagger
- * /prestataire/livreDOr :
+ * /livreDOr :
  *      post :
  *          description : Used to add a comment to the livreDOr
  *          tags :
  *              - prestataire
- *              - livreDOr
  *          parameters : none
  *          responses :
- *              - 200 : comment added with success
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.post("/livreDOR", prestataireController.livreDOr);
 /**
  * @swagger
- * /prestataire/achatBillet :
+ * /achatBillet :
  *      post :
  *          description : Used to add a billet in the table billet with the corresponding user
  *          tags :
  *              - prestataire
- *              - statistique
  *          parameters :
  *              - id wich represent the id of the selected stats for the corresponding prestataire
  *          responses :
- *              - 200 : ressources récupérer avec succès
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.post("/achatBillet", prestataireController.achatBillet);
 /**
  * @swagger
- * /prestataire/showGoodies :
+ * /showGoodies :
  *      get :
  *          description : Used to show all the available goodies proposed by the corresponding prestataire
  *          tags :
  *              - prestataire
- *              - goodies
  *          parameters : none
  *          responses :
- *              - 200 : ressources récupérer avec succès
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.get("/showGoodies", prestataireController.showGoodies);
 /**
  * @swagger
- * /prestataire/selectGoodie :
+ * /selectGoodie :
  *      get :
  *          description : Used to add the selected goodie to the table panier
  *          tags :
  *              - prestataire
- *              - goodies
- *              - panier
  *          parameters : none
  *          responses :
- *              - 200 : ressources récupérer avec succès
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.post("/selectGoodie", prestataireController.selectGoodies);
 /**
  * @swagger
- * /prestataire/buyGoodies :
+ * /buyGoodies :
  *      get :
  *          description : Used to pass the command of the selected goodies for the corresponding client
  *          tags :
  *              - prestataire
- *              - goodies
- *              - panier
  *          parameters : none
  *          responses :
- *              - 200 : ressources récupérer avec succès
- *              - 400 : erreur de requête
- *              - 500 : erreur coté serveur
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
  */
 router.get("/buyGoodies", prestataireController.buyGoodies);
 
